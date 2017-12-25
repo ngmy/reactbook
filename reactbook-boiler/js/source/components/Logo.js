c622fc39a2519eea194dceda97933ab8c97d0874
Yuta Nagamiya <y.nagamiya@gmail.com>
2017-12-25 18:21:44 +0900

    5.5まで写経

---
+++ reactbook-boiler/js/source/components/Logo.js
@@ -0,0 +1,16 @@
+//var React = require('react');
+import React from 'react';
+
+//var Logo = React.createClass({
+//  render: function() {
+//    return <div className="Logo">
+//  }
+//});
+class Logo extends React.Component {
+  render() {
+    return <div className="Logo" />;
+  }
+}
+
+//module.exports = Logo;
+export default Logo
