1263bc23113bba168755c1cfecd84f6474a29fa8
Yuta Nagamiya <y.nagamiya@gmail.com>
2017-12-26 16:01:29 +0900

    7.1.2まで写経

--- whinepad2/scripts/build.sh
+++ whinepad2/scripts/build.sh
@@ -1,4 +1,4 @@
-babel --presets react,es2015 js/source -d js/build
+babel js/source -d js/build
 browserify js/build/app.js -o bundle.js
 browserify js/build/discover.js -o discover-bundle.js
 cat css/*/* css/*.css | sed 's/..\/..\/images/images/g' > bundle.css
