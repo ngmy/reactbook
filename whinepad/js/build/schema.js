aff86254a4f86c6a237af0aa66912369ee7ade36
Yuta Nagamiya <y.nagamiya@gmail.com>
2017-12-26 13:33:22 +0900

    6.3まで写経

---
+++ whinepad/js/build/schema.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var _classification = require('./classification');
+
+var _classification2 = _interopRequireDefault(_classification);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
